4aa3690dbaa7a573a13186d16d5cb811ddf55f69
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-02-15 11:34:35 +0100

    add body-parser middleware for parsing json from twilio

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.listen(port, (err) => {
   if (err) {
