4d3134c91cc7bb87d1d4239e0ce642dbde223386
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-02-15 12:14:44 +0100

    post to Slack

--- src/controllers/twilio-controller.js
+++ src/controllers/twilio-controller.js
@@ -1,5 +1,25 @@
+const Slack = require('slack-node');
+
+const token = process.env.SLACK_TOKEN || '';
+const slackChannel = process.env.SLACK_CHANNEL || '#general';
+
+const slackClient = new Slack(token);
+
 module.exports = {
   postReceived: function(req, res) {
+    const sms = req.body;
+    const smsContent = sms.Body;
+    const smsSender = sms.From;
+
+    slackClient.api('chat.postMessage', {
+      text: `Twilio message: ${smsContent} -- from ${smsSender}`,
+      channel: slackChannel,
+    }, function(err, response) {
+      if (err) {
+        console.error(err);
+      }
+    });
+
     res.status(204).send();
   }
 };
