e4b9179990ad8f00919c04ed66d08c9fc7ad6687
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-02-16 16:18:14 +0100

    make domain restriction on google auth optional

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -2,6 +2,16 @@ const router = require('express').Router();
 const passport = require('passport');
 
 module.exports = function() {
+  const authenticateConfig = {
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email'
+    ]
+  };
+  if (process.env.EMAIL_DOMAIN_RESTRICTION) {
+    authenticateConfig.hd = process.env.EMAIL_DOMAIN_RESTRICTION;
+  }
+
   router.route('/google/callback')
     .get(passport.authenticate('google', {
       successRedirect: '/messages',
@@ -9,13 +19,7 @@ module.exports = function() {
     }));
 
   router.route('/google')
-    .get(passport.authenticate('google', {
-      scope: [
-        'https://www.googleapis.com/auth/userinfo.profile',
-        'https://www.googleapis.com/auth/userinfo.email'
-      ],
-      hd: 'netguru.pl',
-    }));
+    .get(passport.authenticate('google', authenticateConfig));
 
   return router;
 };
