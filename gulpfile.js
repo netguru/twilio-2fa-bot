b77cb8fa578063a90585d8613ab7b749b99b81da
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-02-17 09:40:11 +0100

    adjust env file for gulp-env

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var env = require('gulp-env');
 
 var jsFiles = ['*.js', '/src/**/*.js'];
 
 gulp.task('serve', function() {
+  env({ file: './.env.ini' });
+
   var options = {
     script: './index.js',
     delayTime: 1,
